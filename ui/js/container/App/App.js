b7dbc6634c7f51521c24d23f88a08109aa2e89cc
Juha Syrjälä <juha.syrjala@nitorcreations.com>
2015-11-12 21:42:05 +0200

    Basic react build with webpack and babel

---
+++ ui/js/container/App/App.js
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (<div>
+            <h1>Hello from react</h1>
+        </div>)
+    }
+}
---
+++ ui/js/willow-main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './routes'
+console.log('willow-main running', document.getElementById('app-root'))
+
+
+ReactDOM.render(
+    <div>
+        <h1>Hello from React</h1>
+    </div>,
+    document.getElementById('app-root')
+);
