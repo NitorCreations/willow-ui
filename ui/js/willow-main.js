1d5285a31ca82305d3a272c100dad45d28501391
Juha Syrjälä <juha.syrjala@nitorcreations.com>
2015-11-14 11:15:00 +0200

    Add redux-devtools

--- ui/js/willow-main.js
+++ ui/js/willow-main.js
@@ -7,16 +7,22 @@ import { Provider } from 'react-redux';
 import { Router, Redirect } from 'react-router';
 import configureStore from './store/configure-store';
 import routes from './routes';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 const store = configureStore();
 
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={createBrowserHistory()}>
-            {routes}
-        </Router>
-    </Provider>
+    <div>
+        <Provider store={store}>
+            <Router history={createBrowserHistory()}>
+                {routes}
+            </Router>
+        </Provider>
+        <DebugPanel top right bottom>
+            <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
+    </div>
     ,
     document.getElementById('app-root')
 );
