cecaab29f755f58d8e11a58e2392385ec0cc8a14
Juha Syrjälä <juha.syrjala@nitorcreations.com>
2015-11-14 17:48:18 +0200

    Add example of background service and startup command

--- ui/js/routes.js
+++ ui/js/routes.js
@@ -5,8 +5,6 @@ import { App, Home, Settings, NotFound } from 'containers';
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={Home}/>
-        <Route path="home" component={Home}/>
-
         <Route path="settings" component={Settings}/>
         <Route path="*" component={NotFound} status={404}/>
     </Route>
