a2c6b8fa90b85dc4834d8a15b677743ecaeb0d8c
Juha Syrjälä <juha.syrjala@nitorcreations.com>
2015-11-15 19:20:25 +0200

    Add redux-simle-router

--- ui/js/reducers/index.js
+++ ui/js/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { counters } from './counters'
+const { routeReducer } = require('redux-simple-router');
 
 const rootReducer = combineReducers({
-    counters
+    counters,
+    routing: routeReducer
 })
 
 export default rootReducer
