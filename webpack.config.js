2b76cfaa6f3498227f3f2cf59a1a10cc4d94a22b
Sami Airaksinen <sami.airaksinen@nitorcreations.com>
2016-02-28 16:58:32 +0200

    add loader for font files, fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -5,52 +5,55 @@ var webpack = require('webpack');
 require('es6-promise').polyfill();
 
 module.exports = {
-    entry: [
-        'webpack-hot-middleware/client?path=/ui/__webpack_hmr', // WebpackDevServer host and port
-        'babel-polyfill',
-        './ui/js/willow-main'
-    ],
-    output: {
-        publicPath: '/ui/',
-        path: path.join(__dirname, '/dist/js'),
-        filename: 'willow-main.js'
-    },
-
-    resolve: {
-        extensions: ['', '.jsx', '.js', '.json'],
-        modulesDirectories: ['node_modules', 'ui/js']
-    },
-    node: {
-        fs: "empty"
-    },
-    module: {
+  entry: [
+    'webpack-hot-middleware/client?path=/ui/__webpack_hmr', // WebpackDevServer host and port
+    'babel-polyfill',
+    './ui/js/willow-main'
+  ],
+  output: {
+    publicPath: '/ui/',
+    path: path.join(__dirname, '/dist/js'),
+    filename: 'willow-main.js'
+  },
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.json'],
+    modulesDirectories: ['node_modules', 'ui/js']
+  },
+  node: {
+    fs: "empty"
+  },
+  module: {
+    loaders: [
+      {
         loaders: [
-            {
-                loaders: [
-                    'react-hot',
-                    'babel?' + JSON.stringify({
-                        plugins: ['transform-runtime'],
-                        presets: ['es2015', 'stage-0', 'react']
-                    })
-                ],
+          'react-hot',
+          'babel?' + JSON.stringify({
+            plugins: ['transform-runtime'],
+            presets: ['es2015', 'stage-0', 'react']
+          })
+        ],
 
-                // Skip any files outside of your project's `src` directory
-                include: [
-                    path.resolve(__dirname, "ui"),
-                ],
+        // Skip any files outside of your project's `src` directory
+        include: [
+          path.resolve(__dirname, "ui"),
+        ],
 
-                // Only run `.js` and `.jsx` files through Babel
-                test: /\.jsx?$/,
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style!css!sass'
-            }
-        ]
-    },
-    devtool: 'cheap-module-source-map',
-    debug: true,
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        // Only run `.js` and `.jsx` files through Babel
+        test: /\.jsx?$/,
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      },
+      {
+        test: /.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader'
+      }
     ]
+  },
+  devtool: 'cheap-module-source-map',
+  debug: true,
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
