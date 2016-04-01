03d31c37d75b85214ee9cedd5e879e07bd13580f
Sami Airaksinen <sami.airaksinen@nitorcreations.com>
2016-04-01 21:37:37 +0300

    Copied existing stub from other branch
    - ref https://github.com/NitorCreations/willow-ui/commit/8f08d4c33be4290eb671f798a5217d8ad2685bdb

--- ui/js/components/HorizonGraph/HorizonGraph.js
+++ ui/js/components/HorizonGraph/HorizonGraph.js
@@ -1,15 +1,72 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import cubism from 'cubism';
+import d3 from 'd3';
 import { ConfigurationDisplay } from 'components';
 import './HorizonGraph.scss';
 
 class HorizonGraph extends Component {
+
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div>{this.props.config.uid()}</div>
+    return (
+      <div id={this.props.config.uid()} className="cubism-graph" ref="graph"></div>
+    )
+  }
+
+  componentDidMount() {
+    function random_ma(name) {
+      return context.metric(function(start,stop,step,callback) {
+        var values = [Math.random()];
+        while ( start < stop -1 ){
+          start += step;
+          values.push( values[values.length-1] + (Math.random()-0.5) * 0.1);
+        }
+        callback(null,values);
+      }, name)
+    }
+
+    const context = cubism.context().size(700)
+    const horizon = context.horizon().extent([0,2])
+    context.step(1000)
+    horizon.metric(random_ma)
+    const metrics = ["X","Y","Z"];
+    d3.select(this.refs.graph).selectAll(".horizon")
+      .data(metrics)
+      .enter()
+      .append("div")
+      .attr("class", "horizon")
+      .call(horizon);
+
+    // set rule
+    d3.select(this.refs.graph).append("div")
+      .attr("class", "rule")
+      .call(context.rule());
+
+    // set focus
+    context.on("focus", function(i) {
+      d3.selectAll(".value")
+        .style( "right", i == null ? null : context.size() - i + "px");
+    });
+
+    // set axis
+    var axis = context.axis()
+    d3.select(this.refs.graph).append("div").attr("class", "axis").append("g").call(axis);
+
+    this.context = context
+    this.horizon = horizon
+  }
+
+  componentWillUnmount() {
+    if (this.horizon) {
+      d3.select(".horizon")
+        .call(this.horizon.remove)
+        .remove();
+      this.horizon = undefined
+    }
   }
 }
 
