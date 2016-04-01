0d2ba8aa115c7483a6b64623f0ccda7fa573a4ea
Sami Airaksinen <sami.airaksinen@nitorcreations.com>
2016-04-01 12:58:16 +0300

    ability to create multiple instaces of the ShellTerminal

--- ui/js/containers/Shell/Shell.js
+++ ui/js/containers/Shell/Shell.js
@@ -3,24 +3,62 @@ import { connect } from 'react-redux';
 import { ShellTerminal } from 'components';
 import './Shell.scss';
 
+function terminalConfigurations(newUser, newHost) {
+  return {
+    user: newUser,
+    host: newHost,
+    rows: 30,
+    maxCols: 120
+  };
+}
+
 class Shell extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      user: '@admin',
-      host: 'draco',
-      rows: 30,
-      maxCols: 120
+      terminals: [],
+      newUser: "@admin",
+      newHost: "hostname"
     };
   }
 
   render() {
+    var terminals = this.state.terminals;
     return (
       <div className="shell-container">
-        <ShellTerminal {...this.state} />
+        <header>
+          <p1>Shells</p1>
+          <form>
+            <input type="text" name="username" value={this.state.newUser} onChange={this.updateUserName.bind(this)} />
+            <input type="text" name="host" value={this.state.newHost} onChange={this.updateHost.bind(this)} />
+            <button onClick={this.handleSubmit.bind(this)}>"create"</button>
+          </form>
+        </header>
+        {terminals.map(terminalConfig => {
+          return <ShellTerminal {...terminalConfig} />
+        })}
       </div>
     )
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      terminals: this.state.terminals.concat(terminalConfigurations(this.state.newUser, this.state.newHost))
+    })
+  }
+
+  updateUserName(event) {
+    this.setState({
+      newUser: event.target.value
+    })
+  }
+
+  updateHost(event) {
+    this.setState({
+      newHost: event.target.value
+    })
   }
 }
 
